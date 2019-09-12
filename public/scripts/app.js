5020064d0e522d7d9d83841ba87a9841a05e47cc
Andrew Duong <duongandrews@gmail.com>
2019-09-12 15:31:45 +0000

    Design change

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,7 +15,9 @@ const createTweetElement = (tweetData) => {
         </div>
         <footer class="footer-container">
           <span class="date">${tweetData.created_at}</span>
-          <span class="symbols">Hello</span>
+          <span class="symbols"><i class="fa fa-flag" aria-hidden="true"></i>
+          <i class="fa fa-retweet" aria-hidden="true"></i>
+          <i class="fa fa-heart" aria-hidden="true"></i></span>
         </footer>
       </article>
   `;
@@ -37,37 +39,14 @@ const loadtweets = () => {
   });
 };
 
-// const data = [{
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-
 $(document).ready(() => {
   $("#new-tweet").on('submit', (event) => {
     event.preventDefault();
     const input = $("#new-tweet").serialize();
-    if (input.length > 145 || input.length === 5 || input === null) {
-      return "Error";
+    if (input.length > 145) {
+      return alert("Too many characters");
+    } else if (input.length === 5 || input === null) {
+      return alert("Cannot be empty");
     } else {
       $.ajax({
         url: `/tweets`,
